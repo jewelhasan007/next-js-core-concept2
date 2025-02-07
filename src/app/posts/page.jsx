2278990e5f70d91c8a6df12e41fb73dab5816b11
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-02-07 21:08:40 +0600

    Environment Variable in NEXT JS

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -8,7 +8,7 @@ export const metadata = {
   };
 
 const page = async () => {
-const  data = await fetch('https://jsonplaceholder.typicode.com/posts')
+const  data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`)
 const posts = await data.json()
 // if(data){
 //     redirect(`/posts/${posts[1].id}`) //redirect the posts page to details page
